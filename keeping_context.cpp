3dd0e7225cde47feb0faf1bfa52f2b37c5565e4a
Torran Elson <torran@condensereality.com>
2022-03-22 15:47:52 +0000

    A solution using shared pointers.

--- keeping_context.cpp
+++ keeping_context.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
 class Context {
 public:
+    using Self = Context;
+    using Ptr = std::shared_ptr<Self>;
+
     Context() {};
+
+    static Ptr New() { return std::make_shared<Self>(); }
 
     Context(const Context &original, const std::string &key, const std::string &value) : map_{original.map_} {
         map_[key] = value;
     }
-    Context* insertNewKeyValue(const std::string &key, const std::string &value) {
-        auto new_context = new Context(*this, key, value);
-        return new_context;
+
+    Ptr insertNewKeyValue(const std::string &key, const std::string &value) {
+        return std::make_shared<Self>(*this, key, value);
     }
 
     const std::string printContext() const {
         std::string output;
         for (const auto&[key, value]: map_)
-            output += key + "=" + value;
+            output += key + "=" + value + " ";
         return output;
     }
 
@@ -28,14 +34,18 @@ private:
 };
 
 namespace cr {
-    void log(Context *ctx, const std::string message) {
-        std::cout << ctx->printContext() << " " << message << "\n";
+    void log(const Context::Ptr ctx, const std::string message) {
+        std::cout << ctx->printContext() << message << "\n";
     }
 }
 
 class Frame {
 public:
+    using Self = Frame;
+    using Ptr = std::shared_ptr<Self>;
+
     Frame(const std::string &id) : id_{id} {}
+    static Ptr New(const std::string &id) { return std::make_shared<Self>(id); }
 
     const std::string &id() const { return id_; }
 
@@ -43,7 +53,7 @@ private:
     const std::string id_;
 };
 
-void processFrame(Context *ctx, Frame *frame) {
+void processFrame(Context::Ptr ctx, const Frame::Ptr frame) {
     // add a call here to add a frame_id to the context
     ctx = ctx->insertNewKeyValue("frame_id", frame->id());
     cr::log(ctx, "started processFrame");
@@ -51,7 +61,7 @@ void processFrame(Context *ctx, Frame *frame) {
     cr::log(ctx, "finished processFrame");
 }
 
-void processAllFrames(Context *ctx, std::vector<Frame *> frames) {
+void processAllFrames(Context::Ptr ctx, const std::vector<Frame::Ptr> frames) {
     // add a call here to add a frame_count value to the context, which should
     // be the length of the frame vector
     ctx = ctx->insertNewKeyValue("frame_count", std::to_string(frames.size()));
@@ -62,18 +72,18 @@ void processAllFrames(Context *ctx, std::vector<Frame *> frames) {
     }
 }
 
-std::vector<Frame *> getFrames() {
-    std::vector<Frame *> frames;
-    for (int i = 0; i < 2; ++i) {
-        auto frame = new Frame(std::to_string(1 + i));
+const std::vector<Frame::Ptr> getFrames() {
+    std::vector<Frame::Ptr> frames;
+    for (int i = 0; i < 2; ++i){
+        const auto frame = Frame::New(std::to_string(1 + i));
         frames.push_back(frame);
     }
     return frames;
 }
 
 int main(int argc, char **argv) {
-    auto frames = getFrames();
-    auto ctx = new Context(); // create a context, somehow
+    const auto frames = getFrames();
+    auto ctx = Context::New(); // create a context, somehow
     processAllFrames(ctx, frames);
     return 0;
 }
